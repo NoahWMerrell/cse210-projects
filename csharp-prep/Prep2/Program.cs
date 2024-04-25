d4623d897da626119c32d924ffdd5b5447681d35
Noah Merrell <noah.weston.merrell@gmail.com>
2024-04-25 13:05:21 -0600

    Finished grade program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        // Ask user for input
+        Console.Write("What is your grade percentage? ");
+        string nmgradeAnswer = Console.ReadLine();
+        int nmgrade = int.Parse(nmgradeAnswer);
+
+        string nmletter;
+        if (nmgrade >= 90)
+        {
+            nmletter = "A";
+        }
+        else if (nmgrade >= 80)
+        {
+            nmletter = "B";
+        }
+        else if (nmgrade >= 70)
+        {
+            nmletter = "C";
+        }
+        else if (nmgrade >= 60)
+        {
+            nmletter = "D";
+        }
+        else
+        {
+            nmletter = "F";
+        }
+        
+        Console.WriteLine($"Grade: {nmletter}");
+        if (nmgrade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class. Better luck next time!");
+        }
     }
 }
