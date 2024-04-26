81154aea9bd481155ff7660b9384ee81fef2159f
Noah Merrell <mer21025@byui.edu>
2024-04-26 10:21:40 -0600

    Added comments for testing

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,8 +5,10 @@ class Program
     static void Main(string[] args)
     {
         // Ask user for input
+        // First name
         Console.Write("What is your first name? ");
         string nmfirst = Console.ReadLine();
+        // Last name
         Console.Write("What is your last name? ");
         string nmlast = Console.ReadLine();
 
