d575432af59ceaa51491af1ff0f68a7e07117b1a
NoahWMerrell <168214327+NoahWMerrell@users.noreply.github.com>
2024-05-01 16:40:47 -0600

    Added Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // string nmNumberStr = Console.ReadLine();
+        // int nmNumber = int.Parse(nmNumberStr);
+
+        // Generate random number
+        Random randomGenerator = new Random();
+        int nmNumber = randomGenerator.Next(1, 101);
+        int nmGuess = 0;
+        while (nmGuess != nmNumber)
+        {
+            // Ask user for guess
+            Console.Write("What is your guess? ");
+            string nmGuessStr = Console.ReadLine();
+            nmGuess = int.Parse(nmGuessStr);
+
+            // Determines how guess compares to number
+            if (nmGuess < nmNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (nmGuess > nmNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        
     }
 }
