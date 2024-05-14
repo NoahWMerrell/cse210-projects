966790e4ae93774d7a5786a54f73d081afd229c3
NoahWMerrell <168214327+NoahWMerrell@users.noreply.github.com>
2024-05-14 15:30:31 -0600

    Began developement on Entry and Prompt class

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,6 +1,34 @@
+using System.Reflection;
+
 public class Entry
 {
-    public string _dateTime;
+    public DateTime _dateTime;
     public string _input;
     public string _prompt;
+
+    public Entry(DateTime dateTime, string input, string prompt)
+    {
+        _dateTime = dateTime;
+        _input = input;
+        _prompt = prompt;
+    }
+
+    public static Entry Create(string input)
+    {
+        DateTime currentDate = DateTime.Now;
+        string prompt = Prompt.Generate();
+        return new Entry(currentDate, input, prompt);
+    }
+
+    public static string GetEntry()
+    {
+        return "test";
+    }
+
+    public static void Display(Entry entry)
+    {
+        Console.WriteLine($"Prompt: {entry._prompt}");
+        Console.WriteLine($"Created: {entry._dateTime}");
+        Console.WriteLine($"{entry._input}");
+    }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,10 +1,38 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
-    static public Journal currentJournal;
+    // public static Journal currentJournal;
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Entry newEntry = Entry.Create("I had a wonderful day!");
+        Entry.Display(newEntry);
+
+        string filePath = "data.txt";
+        SaveJournal(filePath, "Hello world!");
+
+        string data = LoadJournal(filePath);
+        Console.WriteLine($"Text read from {filePath}: {data}");
+    }
+
+    // Saves Journal to text file
+    static void SaveJournal(string filePath, string data)
+    {
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            writer.WriteLine(data);
+        }
+    }
+
+    // Returns string of information contained in text file
+    static string LoadJournal(string filePath)
+    {
+        string data = "";
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            data = reader.ReadToEnd();
+        }
+        return data;
     }
 }
