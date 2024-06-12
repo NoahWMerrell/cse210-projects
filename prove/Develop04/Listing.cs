21c99e755742be7f601e4609cd5a75a575ebc7ec
NoahWMerrell <168214327+NoahWMerrell@users.noreply.github.com>
2024-06-12 08:33:38 -0600

    Edited comments

--- prove/Develop04/Listing.cs
+++ prove/Develop04/Listing.cs
@@ -12,7 +12,7 @@ public class Listing : Activity
     // Constructor
     public Listing() : base("Listing", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.") {}
 
-    // Runs the Reflecting activity
+    // Runs the Listing activity
     public void RunListing()
     {
         DisplayWelcome();
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,7 +5,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Animation(5);
         MenuLoop();
     }
 
