d03f2f664dd915ee5be3f43c62e582756c6ed625
NoahWMerrell <168214327+NoahWMerrell@users.noreply.github.com>
2024-07-08 08:58:21 -0600

    Testing

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.Clear();
-        Check check = new Check(0, 2, 0);
+        Check check = new Check(0, 0, 0);
         CheckSimulation sim = new CheckSimulation(1000000, check);
         sim.Run();
     }
